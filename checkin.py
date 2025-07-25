f22e7a85d5354975d652e96c4e0567b51776544a
shenghui_niu <54350593+YvGuo-YY@users.noreply.github.com>
2025-07-26 01:37:54 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,27 +1,197 @@
-import requests
 import json
 import os
+import re
+import sys
+import requests
 
-# from pypushdeer import PushDeer
+cookie_list = os.getenv("COOKIE_QUARK").split('\n|&&')
+sckey = os.environ.get("SENDKEY", "")
 
-# -------------------------------------------------------------------------------------------
-# github workflows
-# -------------------------------------------------------------------------------------------
-if __name__ == '__main__':
+
+# 获取环境变量
+def get_env():
+    # 判断 COOKIE_QUARK是否存在于环境变量
+    if "COOKIE_QUARK" in os.environ:
+        # 读取系统变量以 \n 或 && 分割变量
+        cookie_list = re.split('\n|&&', os.environ.get('COOKIE_QUARK'))
+    else:
+        # 标准日志输出
+        pass
+        # 脚本退出
+        # sys.exit(0)
+
+    cookie_list=[
+            'user=yvguo;kps=LuHy8wDz01gpt3RU3MtdyDNQpd7mVPQg3IKrlAI2EE00sh%2BJkC0WpaJ2kSfI%2FphkEnE2ekQc4SLE%2F70ItJDVvHauTm7QqYXHSnr6uTP1uNKDmw%3D%3D; sign=LuEHY97vPjMvTN6JqiQ2RCn8mT%2BwTPoPvcT6k3WdrIRoa9iM7xpYEEIXvBVfhkqyl4w%3D; vcode=1753462197971']
+
+    return cookie_list
+
+# 其他代码...
+class Quark:
+    '''
+    Quark类封装了签到、领取签到奖励的方法
+    '''
+    def __init__(self, user_data):
+        '''
+        初始化方法
+        :param user_data: 用户信息，用于后续的请求
+        '''
+        self.param = user_data
+
+    def convert_bytes(self, b):
+        '''
+        将字节转换为 MB GB TB
+        :param b: 字节数
+        :return: 返回 MB GB TB
+        '''
+        units = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+        i = 0
+        while b >= 1024 and i < len(units) - 1:
+            b /= 1024
+            i += 1
+        return f"{b:.2f} {units[i]}"
+
+    def get_growth_info(self):
+        '''
+        获取用户当前的签到信息
+        :return: 返回一个字典，包含用户当前的签到信息
+        '''
+        url = "https://drive-m.quark.cn/1/clouddrive/capacity/growth/info"
+        querystring = {
+            "pr": "ucpro",
+            "fr": "android",
+            "kps": self.param.get('kps'),
+            "sign": self.param.get('sign'),
+            "vcode": self.param.get('vcode')
+        }
+        response = requests.get(url=url, params=querystring).json()
+        #print(response)
+        if response.get("data"):
+            return response["data"]
+        else:
+            return False
+
+    def get_growth_sign(self):
+        '''
+        获取用户当前的签到信息
+        :return: 返回一个字典，包含用户当前的签到信息
+        '''
+        url = "https://drive-m.quark.cn/1/clouddrive/capacity/growth/sign"
+        querystring = {
+            "pr": "ucpro",
+            "fr": "android",
+            "kps": self.param.get('kps'),
+            "sign": self.param.get('sign'),
+            "vcode": self.param.get('vcode')
+        }
+        data = {"sign_cyclic": True}
+        response = requests.post(url=url, json=data, params=querystring).json()
+        #print(response)
+        if response.get("data"):
+            return True, response["data"]["sign_daily_reward"]
+        else:
+            return False, response["message"]
+
+    def queryBalance(self):
+        '''
+        查询抽奖余额
+        '''
+        url = "https://coral2.quark.cn/currency/v1/queryBalance"
+        querystring = {
+            "moduleCode": "1f3563d38896438db994f118d4ff53cb",
+            "kps": self.param.get('kps'),
+        }
+        response = requests.get(url=url, params=querystring).json()
+        # print(response)
+        if response.get("data"):
+            return response["data"]["balance"]
+        else:
+            return response["msg"]
+
+    def do_sign(self):
+        '''
+        执行签到任务
+        :return: 返回一个字符串，包含签到结果
+        '''
+        log = ""
+        # 每日领空间
+        growth_info = self.get_growth_info()
+        if growth_info:
+            log += (
+                f" {'88VIP' if growth_info['88VIP'] else '普通用户'} {self.param.get('user')}\n"
+                f"💾 网盘总容量：{self.convert_bytes(growth_info['total_capacity'])}，"
+                f"签到累计容量：")
+            if "sign_reward" in growth_info['cap_composition']:
+                log += f"{self.convert_bytes(growth_info['cap_composition']['sign_reward'])}\n"
+            else:
+                log += "0 MB\n"
+            if growth_info["cap_sign"]["sign_daily"]:
+                log += (
+                    f"✅ 签到日志: 今日已签到+{self.convert_bytes(growth_info['cap_sign']['sign_daily_reward'])}，"
+                    f"连签进度({growth_info['cap_sign']['sign_progress']}/{growth_info['cap_sign']['sign_target']})\n"
+                )
+            else:
+                sign, sign_return = self.get_growth_sign()
+                if sign:
+                    log += (
+                        f"✅ 执行签到: 今日签到+{self.convert_bytes(sign_return)}，"
+                        f"连签进度({growth_info['cap_sign']['sign_progress'] + 1}/{growth_info['cap_sign']['sign_target']})\n"
+                    )
+                else:
+                    log += f"❌ 签到异常: {sign_return}\n"
+        else:
+            log += f"❌ 签到异常: 获取成长信息失败\n"
+
+        return log
+
+
+def main():
+    '''
+    主函数
+    :return: 返回一个字符串，包含签到结果
+    '''
+    msg = ""
+    global cookie_quark
+    cookie_quark = get_env()
+
+    i = 0
+    while i < len(cookie_quark):
+        # 获取user_data参数
+        user_data = {}  # 用户信息
+        for a in cookie_quark[i].replace(" ", "").split(';'):
+            if not a == '':
+                user_data.update({a[0:a.index('=')]: a[a.index('=') + 1:]})
+        # print(user_data)
+        # 开始任务
+        log = f"🙍🏻‍♂️ 第{i + 1}个账号"
+        msg += log
+        # 登录
+        log = Quark(user_data).do_sign()
+        msg += log + "\n"
+
+        i += 1
+
+    # print(msg)
+
+    return msg[:-1]
+def checkin_glados():
+    '''
+    执行签到任务
+    :return: 返回一个字符串，包含签到结果
+    '''
     # pushdeer key 申请地址 https://www.pushdeer.com/product.html
-    sckey = os.environ.get("SENDKEY", "")
+
 
     # 推送内容
     title = ""
-    success, fail, repeats = 0, 0, 0        # 成功账号数量 失败账号数量 重复签到账号数量
+    success, fail, repeats = 0, 0, 0  # 成功账号数量 失败账号数量 重复签到账号数量
     context = ""
 
     # glados账号cookie 直接使用数组 如果使用环境变量需要字符串分割一下
     cookies = os.environ.get("COOKIES", []).split("&")
     if cookies[0] != "":
 
-        check_in_url = "https://glados.space/api/user/checkin"        # 签到地址
-        status_url = "https://glados.space/api/user/status"          # 查看账户状态
+        check_in_url = "https://glados.space/api/user/checkin"  # 签到地址
+        status_url = "https://glados.space/api/user/status"  # 查看账户状态
 
         referer = 'https://glados.space/console/checkin'
         origin = "https://glados.space"
@@ -29,21 +199,22 @@ if __name__ == '__main__':
         payload = {
             'token': 'glados.one'
         }
-        
+
         for cookie in cookies:
             checkin = requests.post(check_in_url, headers={'cookie': cookie, 'referer': referer, 'origin': origin,
-                                    'user-agent': useragent, 'content-type': 'application/json;charset=UTF-8'}, data=json.dumps(payload))
+                                                           'user-agent': useragent,
+                                                           'content-type': 'application/json;charset=UTF-8'},
+                                    data=json.dumps(payload))
             state = requests.get(status_url, headers={
-                                'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent})
+                'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent})
 
             message_status = ""
             points = 0
             message_days = ""
-            
-            
+
             if checkin.status_code == 200:
                 # 解析返回的json数据
-                result = checkin.json()     
+                result = checkin.json()
                 # 获取签到结果
                 check_result = result.get('message')
                 points = result.get('points')
@@ -54,7 +225,7 @@ if __name__ == '__main__':
                 leftdays = int(float(result['data']['leftDays']))
                 # 获取账号email
                 email = result['data']['email']
-                
+
                 print(check_result)
                 if "Checkin! Got" in check_result:
                     success += 1
@@ -75,25 +246,46 @@ if __name__ == '__main__':
                 message_status = "签到请求URL失败, 请检查..."
                 message_days = "error"
 
-            context += "账号: " + email + ", P: " + str(points) +", 剩余: " + message_days + " | "
+            context += "账号: " + email + ", P: " + str(points) + ", 剩余: " + message_days + " | "
 
-        # 推送内容 
-        title = f'Glados, 成功{success},失败{fail},重复{repeats}'
+        # 推送内容
+        context = f'Glados, 成功{success},失败{fail},重复{repeats}' + "\n" + context
         print("Send Content:" + "\n", context)
-        
+
     else:
-        # 推送内容 
+        # 推送内容
         title = f'# 未找到 cookies!'
 
     print("sckey:", sckey)
     print("cookies:", cookies)
-    
+    return context
+def send(msg):
+    '''
+    发送推送消息
+    :param msg:
+    :return: None
+    '''
+    # pushdeer key 申请地址 https://www.pushdeer.com/product.html
     # 推送消息
     # 未设置 sckey 则不进行推送
     if not sckey:
         print("Not push")
     else:
-        # pushdeer = PushDeer(pushkey=sckey) 
+        # pushdeer = PushDeer(pushkey=sckey)
         # pushdeer.send_text(title, desp=context)
-        requests.get(f'{sckey}{title}/{context}')
+        requests.get(f'{sckey}{msg}')
 
+
+# from pypushdeer import PushDeer
+
+# -------------------------------------------------------------------------------------------
+# github workflows
+# -------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    glados_msg = checkin_glados()
+    msg = main()
+    send("签到信息汇总/"+f"""
+glados签到：
+{glados_msg}
+夸克签到：
+{msg}""")
